fbdcd6ef96c1d34031d8c6b629ffce51834eb3b7
Patryk <patryk.sanecki.ux@gmail.com>
2024-06-22 12:09:48 +0200

    Dodaj pliki

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,6 @@ const APP_ID = process.env.REACT_APP_EDAMAM_APP_ID;
 const APP_KEY = process.env.REACT_APP_EDAMAM_APP_KEY;
 
 export const fetchRecipes = async (query) => {
-  const response = await axios.get(`https://api.edamam.com/search?q=${query}&app_id=1a6a2f66&app_key=0b323457046d0a36e07ae5f49201971c`);
+  const response = await axios.get(`https://api.edamam.com/search?q=${query}&app_id=1a6a2f66&app_key='0b323457046d0a36e07ae5f49201971c'`);
   return response.data.hits;
 };
